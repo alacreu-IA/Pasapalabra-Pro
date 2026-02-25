ccb145f64e11ce8053c12891b1b117268698f955
alacreu-IA <andrea.alacreu@gmail.com>
2026-02-25 13:29:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,26 @@
-const CACHE_NAME = "ppro-v1";
+const CACHE_NAME = "ppro-cache";
 
-const CORE_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
-];
-
-// Instalación
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(CORE_FILES);
-    })
-  );
 });
 
-// Activación
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
-// Interceptar peticiones
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.open(CACHE_NAME).then(cache => {
+      return fetch(event.request)
+        .then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        })
+        .catch(() => {
+          return cache.match(event.request);
+        });
     })
   );
 });
